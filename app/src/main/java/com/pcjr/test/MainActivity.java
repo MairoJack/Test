8d4bae702af6b90ee1dfd44436869629b6c38eea
Zhehai <zhehaizhu@gmail.com>
2017-09-04 19:17:01 -0700

    Add Log

--- app/src/main/java/com/pcjr/test/MainActivity.java
+++ app/src/main/java/com/pcjr/test/MainActivity.java
@@ -2,12 +2,17 @@ package com.pcjr.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Print();//new
         setContentView(R.layout.activity_main);
+    }
+    private void Print() {
+        Log.d("myTag", "This is my message");
     }
 }
